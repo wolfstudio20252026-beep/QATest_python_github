fce23a1ed4ff25082cbbef38219ddb42382a79b4
Prokhor <wolfstudio.2025.2026@gmail.com>
2026-09-19 12:47:06 +0300

    Add first_test and second_test

--- test_button.py
+++ test_button.py
@@ -14,3 +14,7 @@ def test_button_exist(browser: webdriver.Chrome):
     browser.get('https://www.qa-practice.com/elements/button/simple')
     assert browser.find_element(By.ID, 'submit-id-submit').is_displayed()
 
+def test_button_exist_2(browser: webdriver.Chrome):
+    browser.get('https://www.qa-practice.com/elements/button/like_a_button')
+    assert browser.find_element(By.PARTIAL_LINK_TEXT, 'Click').is_displayed()
+
